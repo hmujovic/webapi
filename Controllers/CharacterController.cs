35371b70d0aebcf951937f38ba41613c3f962df0
Haris Mujovic <harismujovic@hotmail.com>
2020-06-30 21:31:55 +0200

    Entry done

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using dotnet_rpg.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Id=1, Name = "Sam" } 
+        };
+
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
---
+++ Controllers/Models/RpgClass.cs
@@ -0,0 +1,9 @@
+namespace dotnet_rpg.Controllers.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
