39a40a83188ec6955abc7fd472270a499c9ef223
Haris Mujovic <harismujovic@hotmail.com>
2020-07-05 18:21:07 +0200

    Implemented AutoFight and Highscore method

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -28,5 +28,17 @@ namespace dotnet_rpg.Controllers
         {
             return Ok(await _fightService.SkillAttack(request));
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Fight(FightRequestDto request)
+        {
+            return Ok(await _fightService.Fight(request));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetHighscore()
+        {
+            return Ok(await _fightService.GetHighscore());
+        }
     }
 }
